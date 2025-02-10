902e64d51ac891eb5bc75682ffa72f9848277a79
Matheus-E-Valente <mvalente@alunos.utfpr.edu.br>
2025-02-09 21:36:23 -0300

    Update Fase2.hpp

--- CavesOfDoom-C++/CavesOfDoom-C++/headers/Fase/Fase2.hpp
+++ CavesOfDoom-C++/CavesOfDoom-C++/headers/Fase/Fase2.hpp
@@ -5,14 +5,15 @@
 
 namespace Fase {
 
-    class Fase2 : public Fase {
-    private:
-        void recuperarJogada(const std::vector<std::string> vectorEntidades, const std::vector<std::string> vectorInfoFase);
-    public:
-        Fase2();
-        ~Fase2();
-        void criarFundo();
-        void criarMapa(const IDs::IDs ID_Mapa);
-    };
+            class Fase2 : public Fase {
+        private:
+            void recuperarJogada(const std::vector<std::string> vectorEntidades, const std::vector<std::string> vectorInfoFase);
+        public:
+            Fase2(const std::vector<std::string> vectorEntidades, const std::vector<std::string> vectorInfoFase, const IDs::IDs ID);
+        	Fase2(const IDs::IDs ID = IDs::IDs::fundo_fase2);
+            ~Fase2();
+            void criarFundo();
+            void criarMapa(const IDs::IDs ID_Mapa);
+        };
 
 }
